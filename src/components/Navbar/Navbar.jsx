ad3fa5e414ffcc5d1918e360d09a0de922094940
Vignesh Duraisamy <vigneshd332@gmail.com>
2022-06-29 01:19:04 +0530

    feat: update meta

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import logo from "../../assets/logo.png";
+import logoReduced from "../../assets/logo-reduced.png";
 import styles from "./Navbar.module.css";
 
 const Navbar = () => {
@@ -6,7 +7,11 @@ const Navbar = () => {
     <div className={styles.NavbarContainer}>
       <div className={styles.Navbar}>
         <div className={styles.navLogoContent}>
-          <img src={logo} alt="logo" className={styles.Logo} />
+          <img
+            src={window.innerWidth <= 500 ? logoReduced : logo}
+            alt="logo"
+            className={styles.Logo}
+          />
         </div>
         <div className={styles.navButtons}>
           <div className={styles.navButton}>HOME</div>
